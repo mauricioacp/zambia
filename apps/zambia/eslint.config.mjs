df1eb1e60906d099b55b9cdf7fc15348b7c504b4
Mauricio Cavallini <56032462+mauricioacp@users.noreply.github.com>
2025-03-08 14:18:17 +0100

    feat: add stricter TypeScript and ESLint configurations

--- apps/zambia/eslint.config.mjs
+++ apps/zambia/eslint.config.mjs
@@ -29,6 +29,57 @@ export default [
   {
     files: ['**/*.html'],
     // Override or add rules here
-    rules: {},
+    rules: {
+      'no-extra-boolean-cast': 'error',
+      'no-case-declarations': 'error',
+      '@typescript-eslint/consistent-type-exports': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/ban-ts-comment': ['off'],
+      '@typescript-eslint/no-explicit-any': ['error'],
+      '@typescript-eslint/no-non-null-assertion': ['error'],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'rxjs-angular-x/prefer-takeuntil': [
+        'error',
+        {
+          checkComplete: false,
+          checkDecorators: ['Component', 'Directive', 'Injectable'],
+          alias: ['takeUntilDestroyed'],
+          checkDestroy: false,
+        },
+      ],
+      'rxjs/ban-observables': ['error'],
+      'rxjs/ban-operators': ['error'],
+      'rxjs/no-connectable': ['error'],
+      'rxjs/no-cyclic-action': ['error'],
+      'rxjs/no-compat': ['error'],
+      'rxjs/no-ignored-replay-buffer': ['error'],
+      'rxjs/no-unsafe-catch': ['error'],
+      'rxjs/no-unsafe-first': ['error'],
+      'rxjs/no-unsafe-switchmap': ['error'],
+      'rxjs/no-async-subscribe': ['error'],
+      'rxjs/no-create': ['error'], //
+      'rxjs/no-ignored-observable': ['error'],
+      'rxjs/no-exposed-subjects': ['error'],
+      'rxjs/no-nested-subscribe': ['error'],
+      'rxjs/no-ignored-notifier': ['error'],
+      'rxjs/no-redundant-notify': ['error'],
+      'rxjs/no-subject-unsubscribe': ['error'],
+      'rxjs/no-unbound-methods': ['error'],
+      'rxjs/no-unsafe-subject-next': ['error'],
+      eqeqeq: ['error', 'smart'],
+      'prefer-const': 'error',
+      'no-unused-expressions': 'error',
+      '@angular-eslint/use-lifecycle-interface': 'error',
+      '@angular-eslint/no-input-rename': 'off',
+      '@angular-eslint/no-empty-lifecycle-method': ['error'],
+
+    },
   },
 ];
